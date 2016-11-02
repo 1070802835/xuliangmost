5deb9728fb6a34c7c2176f4633bcf898b7b780a0
xuliangmost <xuliang.most@gmail.com>
2016-11-02 22:03:23 +0800

    11-02-class-night-last

--- JQ/My project/js/Top2.js
+++ JQ/My project/js/Top2.js
@@ -1,3 +1,80 @@
 /**
  * Created by Administrator on 2016/11/2.
  */
+$("#yg").hover(function () {
+	$(this).stop().css({
+		background:"white",
+		border:"1px solid #dfdedf",
+		borderTop:0
+	}).animate({
+		height:"64px"
+	},300)
+},function () {
+	$(this).stop().css({
+		background:"#f3f3f5",
+		border:"0"
+	}).animate({
+		height:"30px"
+	},200)
+});
+
+
+$("#ul-right li").eq(2).hover(function () {
+
+	$("#gg").slideDown("fast");
+	$(this).css({
+		background:"white",
+		border:"1px solid #dfdedf",
+		borderBottom:0
+	})
+},function () {
+	$("#gg").css("display","none");
+	$(this).css({
+		background:"#f3f3f5",
+		border:"0"
+	})
+});
+
+$("#ul-right li").eq(3).hover(function () {
+	$("#gd").slideDown("fast");
+	$(this).css({
+		background:"white",
+		border:"1px solid #dfdedf",
+		borderBottom:0
+	})
+},function () {
+	$("#gd").css("display","none");
+	$(this).css({
+		background:"#f3f3f5",
+		border:"0"
+	})
+});
+
+$("#ul-left li").eq(1).hover(function () {
+	// alert(1)
+	$(this).children().css("font-size","10px").html("Seoul Station");
+},function () {
+	$(this).children().html("首尔站");
+});
+
+
+$("#ul-left li").eq(2).hover(function () {
+	$("#Code").slideDown("fast");
+	$(this).css({
+		background:"white",
+		borderBottom:0
+	})
+},function () {
+	$("#Code").css("display","none");
+	$(this).css({
+		background:"#f3f3f5"
+	})
+});
+$("#top2-right-shop").hover(function () {
+	$(this).css("borderBottom",0);
+	$("#top2-right-shop2").show();
+},function () {
+	$(this).css("borderBottom","1px solid #d1d3d4");
+	$("#top2-right-shop2").hide();
+})
+
