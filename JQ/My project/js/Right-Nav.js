1bf8e6b3c90e7eded7afdbbe97489ecfbb208b1b
xuliangmost <xuliang.most@gmail.com>
2016-11-04 20:57:50 +0800

    11-04-class-night-last

---
+++ JQ/My project/js/Right-Nav.js
@@ -0,0 +1,20 @@
+
+var flag=true;
+$("#rightNav-left .bibo").click(function () {
+	if(flag){
+		$("#rightNav").stop().animate({
+			right:0
+		},400)
+	}else{
+		$("#rightNav").stop().animate({
+			right:"-382px"
+		},400)
+	}
+	flag=!flag;
+});
+var tar1={
+	scrollTop:0
+}
+$("#rightNav-left .biubiubiu1").click(function () {
+	$("body,html").animate(tar1,4000,"easeOutBounce")
+});
