1ccee7885753f53b7b8a762fed2ffd490e8947fa
xuliangmost <xuliang.most@gmail.com>
2016-11-11 18:38:16 +0800

    11-11-class-after-02

--- JQ/My project/js/detail-page.js
+++ JQ/My project/js/detail-page.js
@@ -1,5 +1,4 @@
 $(".Mid-second-ul2-div").hover(function () {
-
 	$(this).css('z-index','5').css('height','28px');
 	$(this).children().last().css({
 		display:'block'
@@ -13,8 +12,57 @@ $(".Mid-second-ul2-div").hover(function () {
 }
 );
 
-// for(var i=0;i<10;i++){
-// 	$("#Mid-second-ul1 li").eq(i).children().css({
-// 		backgroundPosition:i*-96+'px '+'0px'
-// 	})
-// }
+for(var i=0;i<10;i++){
+	$("#Mid-second-ul1 li").eq(i).children().css({
+		backgroundPosition:i*-92+'px '+'0px'
+	})
+}
+
+
+
+$("#Mid-last-ul li").hover(function () {
+		$(this).children().first().css('z-index','4');
+		$(this).children().last().css({
+			display:'block'
+		});
+	}
+	,function () {
+		$(".Mid-last-a").css('z-index','2');
+		$(this).children().last().css({
+			display:'none'
+		});
+	}
+);
+
+var url="json/detail-List.json";
+var setting={
+	dataType:'json',
+	success:function (data) {
+		console.log(data.most);
+		var html=template("List",data.most);
+		$("#detailList").html(html);
+	},
+	error:function () {
+		alert(1)
+	}
+};
+$.ajax(url,setting);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
